9b8fea4100c60cd2deea5ac1d1a4cacfd069fea6
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-11-11 23:11:03 -0300

    Iniciando o Cosumo da API com os nomes de cada pokemon

--- Projeto_Pokedex/assets/main.js
+++ Projeto_Pokedex/assets/main.js
@@ -3,8 +3,35 @@ const limit = 10
 
 const url = ' https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}'
 
-fetch(url).then((response) =>{
-    return response.json()
-}).catch((error)=>{
+fetch(url)
+.then((response) =>response.json())
+.then((jsonBody) => jsonBody.results)
+.then((pokemonList) => { 
+    for (let index = 0; index < pokemonList.length; index++) {
+        const pokemon = pokemonList[index];
+        console.log(convertPokemonToHtml(pokemon))
+
+        document.getElementById('pokemonList').innerHTML += convertPokemonToHtml(pokemon)
+        
+    }    
+})
+.catch((error)=>{
     console.log(error)
 })
+
+function convertPokemonToHtml(pokemon){
+    return `
+    <li class="pokemon grass">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li>Grass</li>
+                <li>Poison</li>
+            </ol>
+        
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        </div>
+    </li>
+    `
+}
