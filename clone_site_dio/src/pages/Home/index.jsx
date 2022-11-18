06e4be3199952c4bd2d759991c817e00a331fe20
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-11-18 18:42:32 -0300

    Adicionando react-router e modificações na fonte

--- clone_site_dio/src/pages/Home/index.jsx
+++ clone_site_dio/src/pages/Home/index.jsx
@@ -1,9 +1,28 @@
+import Button from "../../components/Button"
 import Header from "../../components/Header"
+import './styles.css'
 
 const Home = () =>{
     return(
         <>
             <Header></Header>
+            <div className="Container">
+                <div>
+                    <div className="Title">
+                        <span className="TitleHighLight">
+                            Implemente<br></br>
+                        </span>
+                        o seu futuro global agora!
+                    </div>
+                    <div className="TitleContent">
+                        Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo desafio profissional evoluindo com uma comunidade de experts!
+                    </div>
+                    <Button title={"Começar"} onClick={()=>null}></Button>
+                </div>
+                <div>
+                    <img src="#" alt="Imagem Principal" />
+                </div>
+            </div>
         </>
     )
 }
