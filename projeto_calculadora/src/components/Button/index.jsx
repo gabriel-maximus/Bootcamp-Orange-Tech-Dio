9ae8462b0035e205bc4a6c0b5292020334ccee75
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-11-25 17:13:51 -0300

    Criando os primeiros componentes

---
+++ projeto_calculadora/src/components/Button/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './styles.css'
+
+export default function Button() {
+  return (
+    <button className='ButtonContainer'>1</button>
+  )
+}
---
+++ projeto_calculadora/src/components/Input/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'  
+
+export default function Input() {
+  return (
+    <div className='InputContainer'>
+      <input className='InputContent'></input>
+    </div>
+  )
+}
