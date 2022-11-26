9c762b25edef8ec2621f00770416bf0158415727
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-11-26 00:28:58 -0300

    Adicionando função de soma, possui bugs

--- projeto_calculadora/src/App.js
+++ projeto_calculadora/src/App.js
@@ -5,12 +5,26 @@ import { useState } from 'react';
 
 function App() {
   const [currentNumber, setCurrentNumber] = useState('0')
-  const addNumber = (number) =>{
-    setCurrentNumber(prev => `${number}${prev === '0' ? '' : prev}` )
+  const [firstNumber, setFirstNumber] = useState('0')
+  
+  const addNumber = (num) =>{
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}` )
   }
   const clear = () =>{
-    setCurrentNumber('0')
+    setCurrentNumber('0');
+    setFirstNumber('0');
   }
+  const somarNumeros = () =>{
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+    }else {
+      const soma = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber = String(soma);
+    }
+  }
+  
+
   return (
     <div className="Container">
       <div className='Content'>
@@ -25,7 +39,7 @@ function App() {
           <Button label='7' onClick={()=> addNumber('7')}/>
           <Button label='8' onClick={()=> addNumber('8')}/>
           <Button label='9' onClick={()=> addNumber('9')}/>
-          <Button label='+' onClick={()=> addNumber('+')}/>
+          <Button label='+' onClick={somarNumeros}/>
         </div>
         <div className='Row'>
           <Button label='4' onClick={()=> addNumber('4')}/>
