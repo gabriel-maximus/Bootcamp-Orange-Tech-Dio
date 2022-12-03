dfa35f935061facfc73a6b46393eb97c375d28ac
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-12-02 23:06:29 -0300

    Instalando pacotes iniciais e fazendo primeiros ajustes

--- wiki_github/src/App.js
+++ wiki_github/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/github.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img width={72} height={72} src={logo} alt='Logo Github'/>
+      Teste
     </div>
   );
 }
