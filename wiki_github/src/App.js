fd228960f226d6ab4997ab9b125c7b2edca17c5b
gabriel-maximus <gabrielmaximus80@gmail.com>
2022-12-02 23:24:11 -0300

    Criando componente de input

--- wiki_github/src/App.js
+++ wiki_github/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import logo from './assets/github.png'
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
       <img width={72} height={72} src={logo} alt='Logo Github'/>
       Teste
+      <Input />
     </div>
   );
 }
---
+++ wiki_github/src/components/Input/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+export default function Input() {
+  return (
+    <div className='InputContainer'>
+        <input className='InputField'></input>
+    </div>
+  )
+}
